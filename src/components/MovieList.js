dfe931331178d0558772f96f984cd610c0c05b87
1sfaccia1 <chad.m.wittman75@gmail.com>
2019-08-25 00:59:54 -0500

    ++

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const MovieList = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [movies, setMovies] = useState([
+    {
+      name: "Harry Potter",
+      price: "$10",
+      id: 231234
+    },
+    {
+      name: "Game of Thrones",
+      price: "$10",
+      id: 2566124
+    },
+    {
+      name: "Inception",
+      price: "$10",
+      id: 231524
+    }
+  ]);
   return (
     <div>
-      <h1>movie</h1>
+      <h1>hello</h1>
+      {movies.map(movie => (
+        <li>{movie.name}</li>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default MovieList
+export default MovieList;
