e99cb3c8b72cdcfab7b8127af88bdff494331ec4
1sfaccia1 <chad.m.wittman75@gmail.com>
2019-08-25 00:32:43 -0500

    add components folder & movelist.js tab

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import MovieList from './components/MovieList'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>hello world</h1>
+      <MovieList />
     </div>
   );
 }
